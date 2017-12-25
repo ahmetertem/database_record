c0870c07fb6cc20deea4253ee2b9879945f308a8
Ahmet Ertem <amadertem@gmail.com>
2017-12-25 21:03:51 +0300

    added `call`

--- src/dbr.php
+++ src/dbr.php
@@ -31,6 +31,14 @@ abstract class dbr
         }
     }
 
+    public function __call($method, $args)
+    {
+        if (isset($this->$method)) {
+            $func = $this->$method;
+            return call_user_func_array($func, $args);
+        }
+    }
+
     protected function parseFields()
     {
         $this->_parsed_fields = array();
